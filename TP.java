171e2fbc1f4cc91a5c36eff1f0a8ac667438f57c
KANAMARLAPUDI VENKATA SUBBARAO <88045527+subbaraokanamarlapudi@users.noreply.github.com>
2023-01-03 21:11:53 +0530

    Add files via upload
    This program is :
               Type casting

---
+++ TP.java
@@ -0,0 +1,18 @@
+public class TP {
+    public static void main(String args[])
+    {
+        // int myInt = 9;
+        // double myDouble = myInt;
+
+        // System.out.println(myInt);
+        // System.out.println(myDouble);
+
+
+        double myDouble = 9.87;
+        int myInt = (int) myDouble;         // Manual casting
+
+        System.out.println(myDouble);
+        System.out.println(myInt);
+    }
+    
+}
