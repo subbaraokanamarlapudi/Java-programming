2ab92f8505e0307be71be13435115a3490f843d6
KANAMARLAPUDI VENKATA SUBBARAO <88045527+subbaraokanamarlapudi@users.noreply.github.com>
2023-01-03 21:08:16 +0530

    Add files via upload
    Basic Hello world world program

---
+++ hello.java
@@ -0,0 +1,10 @@
+// import java.util.Scanner;
+public class hello
+{
+    public static void main(String args[])
+    {
+        System.out.println("HELLO WORLD");
+        System.out.println("KVSR");
+        System.out.println("LNIS");   // It also perform mathematical part like -->(2+5)
+    }
+}
